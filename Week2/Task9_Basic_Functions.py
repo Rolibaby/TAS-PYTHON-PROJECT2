612246bef418833d8080b07d48baf6fd14159cd1
Rolibaby <36302871+Rolibaby@users.noreply.github.com>
2022-10-08 03:04:01 +0100

    Update Task10_Anonymous_Functions.py to Task16_Basic_Dictionary_Operations.py

--- Week2/Task9_Basic_Functions.py
+++ Week2/Task9_Basic_Functions.py
@@ -2,17 +2,16 @@
 def greet():
     print("Hello World")
 
+
 def wellcome():
     print("Thank You")
     print("See you tomorrow")
+
 
 def login():
     pass  # Create a function that does nothing, an empty function
 
 
-
 greet()
 wellcome()
-login()    # Create a function that does nothing, an empty function
-
-
+login()  # Create a function that does nothing, an empty function
